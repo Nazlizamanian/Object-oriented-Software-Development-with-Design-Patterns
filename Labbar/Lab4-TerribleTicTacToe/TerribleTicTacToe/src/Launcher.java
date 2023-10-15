528a8210d71f129c1e540359e5be197e31316c99
Nazli Zamanian <nazlizamanian@gmail.com>
2023-10-15 19:01:20 +0200

    Delete Labbar/Lab4-TerribleTicTacToe directory

--- Labbar/Lab4-TerribleTicTacToe/TerribleTicTacToe/src/Launcher.java
+++
@@ -1,8 +0,0 @@
-// Launcher class containing the main method to start the application
-public class Launcher {
-    
-    public static void main(String[] args) {
-        // Creating instances of Board and GameView, and initializing the Controller
-        new Controller(new Board(), new GameView());
-    }
-}

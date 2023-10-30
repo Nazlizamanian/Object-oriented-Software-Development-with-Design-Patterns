5e2c2af4bcda312f5ba2468bd740d3d9e4edede0
Nazli Zamanian <nazlizamanian@gmail.com>
2023-10-30 20:42:17 +0100

    Add files via upload

---
+++ Labbar/Lab03-MVC application/src/invalidInput.java
@@ -0,0 +1,11 @@
+//Handles our invalidInput, delegated work from Controller.
+public class invalidInput{
+    public static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
